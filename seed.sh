5254a123d8c8e988dfd9bcccb8cf5df04af59661
Ian Edwards <isedwards@users.noreply.github.com>
2017-08-31 10:53:49 +0100

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -9,7 +9,7 @@ else
   # Add customisations
   echo >> ~/.bashrc
   echo '## local customisations' >> ~/.bashrc
-  echo "export PS1=($1)"'"${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "' >> ~/.bashrc
+  echo 'export PS1="('"$1"')${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "' >> ~/.bashrc
   echo 'alias proj="cd /media/sf_data/work/projects/"' >> ~/.bashrc
   echo 'alias ..="cd .." ...="cd ../.." ....="cd ../../.." .....="cd ../../../.."' >> ~/.bashrc
 fi
