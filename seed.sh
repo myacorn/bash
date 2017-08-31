03f86f0e45dc416affeca481ce0e177c9741e7b0
Ian Edwards <isedwards@users.noreply.github.com>
2017-08-31 10:41:39 +0100

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -3,7 +3,12 @@
 # curl http://path/to/seed.sh | bash -s proj_name
 
 # Check whether customisatations already exist in ~/.bashrc
-
-# Add customisations
-echo 'export PS1=($1)"${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "' >> ~/.bashrc
-echo 'alias ..="cd .." ...="cd ../.." ....="cd ../../.." .....="cd ../../../.."' >> ~/.bashrc
+if grep -q "local customisations" ~/.bashrc; then
+  echo "Customisation already exist, aborting."
+else
+  # Add customisations
+  echo '\n\n## local customisations' >> ~/.bashrc
+  echo 'export PS1=($1)"${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "' >> ~/.bashrc
+  echo 'alias proj="cd /media/sf_data/work/projects/"' >> ~/.bashrc
+  echo 'alias ..="cd .." ...="cd ../.." ....="cd ../../.." .....="cd ../../../.."' >> ~/.bashrc
+fi
