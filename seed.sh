f0e68134b6af31bc3a7254beacecb4c7cb115f93
Ian Edwards <isedwards@users.noreply.github.com>
2017-09-03 14:13:01 +0100

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Basic server setup, requires `server_name` as an argument
+# Basic server setup, requires `server_name` as an argument to customize shell prompt
 #
 # Example:
 #     curl https://raw.githubusercontent.com/myacorn/bash/master/seed.sh | bash -s server_name
 
-# Check whether customisatations already exist in ~/.bashrc
-if grep -q "local customisations" ~/.bashrc; then
-  echo "Customisation already exist, aborting."
+# Check whether customizatations already exist in ~/.bashrc
+if grep -q "local customizations" ~/.bashrc; then
+  echo "Customization already exist, aborting."
 else
-  # Add customisations
+  # Add customizations
   echo >> ~/.bashrc
-  echo '## local customisations' >> ~/.bashrc
+  echo '## local customizations' >> ~/.bashrc
   echo 'export PS1="['"$1"']${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "' >> ~/.bashrc
   echo 'alias ..="cd .." ...="cd ../.." ....="cd ../../.." .....="cd ../../../.."' >> ~/.bashrc
   
