4be0395725404f5905e8951ae2c0f69b7fcc231c
Ian Edwards <isedwards@users.noreply.github.com>
2017-09-06 10:24:03 +0100

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -13,7 +13,5 @@ else
   echo '## local customizations' >> ~/.bashrc
   echo 'export PS1="['"$1"']${debian_chroot:+($debian_chroot)}\u@\h:\w\$ "' >> ~/.bashrc
   echo 'alias ..="cd .." ...="cd ../.." ....="cd ../../.." .....="cd ../../../.."' >> ~/.bashrc
-  
-  # sesh is a shortcut to ssh to a machine on our local network by supplying just the final part of the IPv4 address
-  sesh() { ssh 192.168.1.$1 $2 $3 $4 $5; }
+  echo 'alias duh="du -h --max-depth=1"' >> ~/.bashrc
 fi
